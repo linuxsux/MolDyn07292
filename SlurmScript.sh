764f4019f44f40334b9a7ccf79d0e03a7c29ef00
linuxsux <146971492+linuxsux@users.noreply.github.com>
2024-07-29 17:50:40 -0400

    Add files via upload

---
+++ SlurmScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=my_gpu_job
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --time=02:00:00
+
+module load StdEnv/2020 gcc/9.3.0 cuda/11.4 openmpi/4.0.3 openmm/7.7.0
+source $HOME/env-parmed/bin/activate
+
+python Test2.py
